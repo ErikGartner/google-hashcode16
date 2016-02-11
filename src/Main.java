9fb8e2f8d424cc76f31a536810c969fdd3ab5680
Erik Gärtner <xxxsmoizxxx@gmail.com>
2016-02-11 18:25:03 +0100

    Add skeleton

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Created by erik on 11/02/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        Main m = new Main();
+        m.solve();
+
+    }
+
+    public void solve() {
+        System.out.println("Klart!");
+    }
+
+
+}
