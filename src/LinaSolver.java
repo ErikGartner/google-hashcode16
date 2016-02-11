f7354dde6c95bebb6f09660bbc2b6e8d29433316
jurgina <carolina.sartorius@gmail.com>
2016-02-11 19:31:02 +0100

    added preprocess orders in LinaSolve

--- src/LinaSolver.java
+++ src/LinaSolver.java
@@ -1,3 +1,19 @@
+import java.util.*;
+
 public class LinaSolver {
+	public void preprocessOrders(List<Order> orders) {
+		for (Order order : orders) {
+			long sum = 0;
+
+			for (Product p : order.products) {
+				sum += order.distanceTo(p.closestWarehouse());
+			}
+
+			order.priority = sum;
+		}
+
+		Collections.sort(orders);
+	}
+
 	
 }
