ffc0bb470182939e80297b4af6b6dd28e549f005
jurgina <carolina.sartorius@gmail.com>
2016-02-11 19:21:48 +0100

    Added order implementation

--- src/Order.java
+++ src/Order.java
@@ -2,4 +2,10 @@
  * Created by erik on 11/02/16.
  */
 public class Order extends MapObject {
+	public int id; 
+	public int priority; 
+
+	public int compareTo(T o) {
+		return priority - o.priority;
+	}	
 }
