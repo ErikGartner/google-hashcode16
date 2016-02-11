1cfeeb956b60e92a6ba792da310bbff9376aea51
Erik Gärtner <xxxsmoizxxx@gmail.com>
2016-02-11 19:14:08 +0100

    Add problem

---
+++ src/Problem.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+/**
+ * Created by erik on 11/02/16.
+ */
+public class Problem {
+
+    public List<Drone> droneList;
+    public List<Order> orderList;
+    public List<Warehouse> warehouseList;
+    public List<Product> productList;
+
+}
