188a85c3727ae76cc9a57470a0396294a881fca4
jurgina <carolina.sartorius@gmail.com>
2016-02-11 20:09:20 +0100

    Added solve method

--- src/LinaSolver.java
+++ src/LinaSolver.java
@@ -15,7 +15,35 @@ public class LinaSolver {
 		Collections.sort(orders);
 	}
 
-	public void solve() {
+	public void solve(Problem p) {
+		preprocessOrders(p.orderList);
 
+		PriorityQueue<Drone> available = new PriorityQueue(p.droneList);
+
+		while (p.currentTurn < p.maxTurns) {
+			Drone d = queue.poll();
+			p.currentTurn = d.availableAt;
+
+			if (d == null) {
+				System.out.println("Empty queue");
+			}
+
+			Order order = p.orderList.get(0);
+
+			Product product = chooseProductToLoad(d, o);
+
+			Warehouse w = chooseWarehouseToGetProduct(d, order, product);
+
+			
+		}
+
+	}
+
+	public Product chooseProductToLoad(Drone d, Order o) {
+		return null;
 	}	
+
+	public Warehouse chooseWarehouseToGetProduct(Drone d, Order order, Product product) {
+		return null;
+	}
 }
