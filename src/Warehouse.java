67287ce470948d7db49b3515e14f8ea0c82cc274
Erik Gärtner <xxxsmoizxxx@gmail.com>
2016-02-11 19:10:29 +0100

    Update warehouse

--- src/Warehouse.java
+++ src/Warehouse.java
@@ -1,5 +1,11 @@
+import java.util.Map;
+
 /**
  * Created by erik on 11/02/16.
  */
 public class Warehouse extends MapObject{
+
+    public int id;
+    public Map<Product, Integer> inventory;
+
 }
