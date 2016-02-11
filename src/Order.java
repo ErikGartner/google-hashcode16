4196a3f59704abaa5bf800b7e3b966aff9b45cfb
jurgina <carolina.sartorius@gmail.com>
2016-02-11 19:25:05 +0100

    Added things to order

--- src/Order.java
+++ src/Order.java
@@ -1,9 +1,12 @@
+import java.util.*;
+
 /**
  * Created by erik on 11/02/16.
  */
 public class Order extends MapObject {
-	public int id; 
-	public int priority; 
+	public int id;
+	public int priority;
+	Map<Product> products;
 
 	public int compareTo(T o) {
 		return priority - o.priority;
